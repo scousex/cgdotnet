f1f214f70e54d0788d11df6fa823ca06006b9013
Ruslan Mustakimov <great.eagle@mail.ru>
2019-10-27 12:39:21 +0700

    initial

---
+++ TestAppGC/Model/Entity/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAppGC.Model.Entity
+{
+    class Person
+    {
+        private string Name;
+        private string Email;
+        private string PhoneNumber;
+        private short GroupId;
+        
+    }
+}
